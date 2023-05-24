55653390cea939c429d96d2f588c2f6b587fbc6b
Ismael Garrido Muñoz <ismael.garrido@liquidbarcodes.com>
2023-05-24 15:24:11 +0200

    'deploy'

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 rmdir /S /Q dist
 call npm i
 call npx ng build modules
-mv .git .git2
+rename .git .git2
 git init
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:RelGarrido/angular-text-mask.git master:npm
 rmdir /S /Q ".git"
-mv .git2 .git
+rename .git2 .git
 
