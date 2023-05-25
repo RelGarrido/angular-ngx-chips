8dfc836e3bac6405705c0abf02c0385321a3b55b
Ismael Garrido Muñoz <ismael.garrido@liquidbarcodes.com>
2023-05-25 08:10:42 +0200

    clean build script

--- build.bat
+++ build.bat
@@ -4,21 +4,18 @@ git add --all
 git commit -m "changes to master"
 git push
 
-pause
 echo 'Building npm branch'
 
 git branch -D npm
 git checkout -b npm
 rmdir /S /Q dist
 rmdir /S /Q node_modules
-pause
 
 call npm i
 call npx ng build modules
 git add --all
 git commit -m "deploy"
 git push -f git@github.com:RelGarrido/angular-text-mask.git npm:npm --force
-pause
 
 rmdir /S /Q dist
 git checkout master
